079d2f57722fa94d80e3836a40f300e1ec6f6258
Luciana <lg.sosa@alumno.um.edu.ar>
2023-08-23 10:16:58 -0300

    cell

--- game_cell.py
+++ game_cell.py
@@ -1,4 +1,18 @@
+from models import Tile
+
 class Cell:
-    def _init_(self, mutiplier, multiplier_type):
-        self.multiplier = mutiplier
+    def __init__(self, multiplier, multiplier_type):
+        self.multiplier = multiplier
         self.multiplier_type = multiplier_type
+        self.letter = None
+
+    def add_letter(self, letter:Tile):
+        self.letter = letter
+
+    def calculate_value(self):
+        if self.letter is None:
+            return 0
+        if self.multiplier_type == 'letter':
+            return self.letter.value * self.multiplier
+        else:
+            return self.letter.value
