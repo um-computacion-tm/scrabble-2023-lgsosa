77448ab045595e8c14ffd5eb90dcacf7e6a33fc5
Luciana <lg.sosa@alumno.um.edu.ar>
2023-08-29 17:33:30 -0300

    intento

--- Test/test_app.py
+++ Test/test_app.py
@@ -1,7 +1,11 @@
 import unittest
+from App.multi import multiplicacion
 class testmulti(unittest.TestCase):
     def test_multi(self):
-       self.assertEqual (multiplicacion(2,3)6)
+        self.assertEqual(multiplicacion(2,3))
+
+
 
 if __name__=="__main__":
     unittest.main()
+
