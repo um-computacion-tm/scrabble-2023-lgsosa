949d577ab7f21d31e24d0d02bedc6e26a39933fc
Luciana <lg.sosa@alumno.um.edu.ar>
2023-09-06 00:34:48 -0300

    validate_word

--- test/test_game_board.py
+++ test/test_game_board.py
@@ -35,6 +35,21 @@ class TestBoard(unittest.TestCase):
         tile = Tile('A', 1)
         self.assertTrue(board.place_tile(7, 7, tile))
         self.assertFalse(board.place_tile(7, 7, tile))
+        
+    def test_validate_word(self):
+        board = Board()
+        tile_a = Tile('A', 1)
+        tile_b = Tile('B', 2)
+        tile_c = Tile('C', 3)
+
+        board.place_tile(7, 7, tile_a)
+        board.place_tile(7, 8, tile_b)
+        board.place_tile(7, 9, tile_c)
+
+        self.assertTrue(board.validate_word(7, 7, 'ABC', 'horizontal'))
+        self.assertFalse(board.validate_word(7, 7, 'ACB', 'horizontal'))
+        self.assertTrue(board.validate_word(7, 7, 'A', 'vertical'))
+        self.assertFalse(board.validate_word(7, 7, 'AB', 'vertical'))
 
 
 if __name__ == '__main__':
