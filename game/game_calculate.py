ec049fef21d81a3e8e315bf7c9eb312d06fb492c
Luciana <lg.sosa@alumno.um.edu.ar>
2023-09-12 19:09:08 -0300

    new class

--- game/game_calculate.py
+++ game/game_calculate.py
@@ -2,14 +2,15 @@ import unittest
 
 from game.game_cell import Cell
 
-def calculate_word_value(word: list[Cell]):
-    value: int = 0
-    multiplier_word = None
-    for cell in word:
-        value = value + cell.calculate_value()
+class calculate_word_value:
+    def calculate_word_value(word: list[Cell]):
+        value: int = 0
+        multiplier_word = None
+        for cell in word:
+            value = value + cell.calculate_value()
         if cell.multiplier_type == 'word':
             multiplier_word = cell.multiplier
             cell.multiplier = 1
-    if multiplier_word != None:
-        value = (value * multiplier_word)
-    return value
+        if multiplier_word != None:
+            value = (value * multiplier_word)
+        return value
