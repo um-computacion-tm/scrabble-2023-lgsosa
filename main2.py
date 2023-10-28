a592509024f2e16099990940b6e1b419f882d389
Luciana <lg.sosa@alumno.um.edu.ar>
2023-10-28 12:53:05 -0300

    the game starts and problems with bagtiles

--- main2.py
+++ main2.py
@@ -13,4 +13,23 @@ class Main:
     def init_game(self):
         self.config.iniciar_juego
         self.config.get_player_count
-        
+        self.config.show_board
+        self.config.show_current_player
+
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.init_game()
+
+    def play_game(self):
+        print('¡VAMOS A JUGAR!')
+        self.initial_tiles()
+        self.game.players[0].rack = [Tile('H', 4), Tile('O',1), Tile('L',1), Tile('A',1)]
+        self.game.show_board(self.board)
+        while not self.game.game_over():
+            self.next_turn()
+            self.show_current_player()
+            self.take_turn()
+        print('¡SE ACABO LO QUE SE DABA!')
+        self.display_final_scores()
